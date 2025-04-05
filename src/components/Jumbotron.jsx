22ce5c066187ca1b9c0070d6e6449bb4c514c672
MeerMuneeb <jimmykuddon@gmail.com>
2025-04-05 16:13:10 +0500

    feat: Add sections  with navigation buttons

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Iphone from "../assets/images/iphone-14.jpg"
+import HoldingIphone from "../assets/images/iphone-hand.png"
 
 function Jumbotron() {
+    const handleLearnMore = () => {
+        const element = document.querySelector(".sound-section")
+        window.scrollTo({
+            top: element?.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
     return (
         <div className="jumbotron-section wrapper">
             <h2 className="title">New</h2>
@@ -10,6 +19,15 @@ function Jumbotron() {
             <span className="description">
                 From $999 or $41.62/mo. for 24 mo. before trade-in
             </span>
+            <ul className="links">
+                <li>
+                    <button className="button">Buy</button>
+                </li>
+                <li>
+                    <a className="link" onClick={handleLearnMore}>Learn More</a>
+                </li>
+            </ul>
+            <img className="iphone-img" src={HoldingIphone} alt="iPhone"/>
         </div>
     );
 }
